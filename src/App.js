ef918fcda0c84bba426ac66c639304777f04823a
Yaelox <yaelsilvestre64@gmail.com>
2023-11-23 14:09:27 -0600

    paginas yael

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import NavbarUta from "./components/Navbar/Navbar";
+import Bolsa from "./Pages/Bolsatrabajo";
+import Exito from "./Pages/exito";
+import Inter from "./Pages/Inter";
+import Vacantes from "./Pages/Vacantes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
@@ -15,6 +19,10 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="contact" element={<HomePage />} />
         <Route path="*" element={<HomePage />} />
+        <Route path="/bolsa" element={<Bolsa/>} />
+        <Route path="/exito" element={<Exito/>} />
+        <Route path="/inter" element={<Inter/>} />
+        <Route path="/vacantes" element={<Vacantes/>} />
       </Routes>
     </div>
   );
